d27ff35268eb439e68122c7631a21a254252190a
pu4ick <98850988+pu4ick@users.noreply.github.com>
2022-02-08 10:46:13 +0100

    Use Destructuring Assignment to Extract Values from Objects finish

--- ES6/use_destructuring_assignment_to_extract_values_from_objects.js
+++ ES6/use_destructuring_assignment_to_extract_values_from_objects.js
@@ -30,5 +30,4 @@ const HIGH_TEMPERATURES = {
   tomorrow: 80
 };
 
-const today = HIGH_TEMPERATURES.today;
-const tomorrow = HIGH_TEMPERATURES.tomorrow;
+const {today, tomorrow} = HIGH_TEMPERATURES;
